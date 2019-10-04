914df1c7d62995b3e6e1e6558ae7fb7c720126eb
Abhay Saxena <ark3@datawire.io>
2019-10-04 11:47:34 -0400

    Add Java service

---
+++ services/java/src/main/java/Main.java
@@ -0,0 +1,56 @@
+import static spark.Spark.get;
+import static spark.Spark.port;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.InetAddress;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.time.Instant;
+import java.util.Base64;
+
+public class Main {
+    static String getValue() {
+        try {
+            URL url = new URL("http://base");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer content = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+            return content.toString();
+        } catch (Exception e) {
+            return e.toString();
+        }
+    }
+
+    static String getHostname() {
+        InetAddress ip;
+        try {
+            ip = InetAddress.getLocalHost();
+            return ip.getHostName();
+        } catch (UnknownHostException e) {
+            return e.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        port(8000);
+        get("/", (req, response) -> {
+            String value = getValue();
+            String hashcode = Base64.getEncoder().encodeToString(value.getBytes());
+            String[] lines = { "[ Hello KubeCon NA 2019! ]", "[ Greetings from Java    ]",
+                    String.format("[ Code: %s ]", hashcode), "", String.format("Host: %s", getHostname()),
+                    String.format("Now:  %s", Instant.now()), };
+
+            String res = String.join("\n", lines) + "\n";
+            response.type("text/plain");
+            return res;
+        });
+    }
+}
