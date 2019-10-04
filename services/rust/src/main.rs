09064209fbd8331dc182e07c5338839ff59fc717
Abhay Saxena <ark3@datawire.io>
2019-10-04 16:17:53 -0400

    Add Rust service

--- services/rust/src/main.rs
+++ services/rust/src/main.rs
@@ -3,9 +3,40 @@
 #[macro_use]
 extern crate rocket;
 
+fn get_value() -> String {
+    let mut response = match reqwest::get("http://base") {
+        Ok(response) => response,
+        Err(e) => return e.to_string(),
+    };
+    let value = match response.text() {
+        Ok(value) => value,
+        Err(e) => return e.to_string(),
+    };
+    return value;
+}
+
+fn get_hostname() -> String {
+    let hostname = match gethostname::gethostname().into_string() {
+        Ok(hostname) => hostname,
+        Err(_) => "Invalid UTF-8".to_string(),
+    };
+    return hostname;
+}
+
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!\n"
+fn index() -> String {
+    let value = get_value();
+    let hashcode = base64::encode(&value);
+    let lines = vec![
+        "[ Hello KubeCon NA 2019! ]".to_string(),
+        "[ Greetings from Rust    ]".to_string(),
+        format!("[ Code: {} ]", hashcode),
+        "".to_string(),
+        format!("Host: {}", get_hostname()),
+        format!("Now:  {}", chrono::Utc::now()),
+    ];
+    let result = lines.join("\n") + "\n";
+    return result;
 }
 
 fn main() {
