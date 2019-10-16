6bd153ece2c6b25bd625652206afd07c292628a1
Abhay Saxena <ark3@datawire.io>
2019-10-16 17:32:57 -0400

    Always return 3 digits before decimal point

--- services/base/base.go
+++ services/base/base.go
@@ -11,7 +11,12 @@ import (
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		now := time.Now()
-		value := float64(now.Unix()%10000) / 10
+		preValue := now.Unix() % 10000
+		if preValue < 1000 {
+			// Get a 4-digit number
+			preValue = 9999 - preValue
+		}
+		value := float64(preValue) / 10
 		fmt.Fprint(w, strconv.FormatFloat(value, 'f', -1, 64))
 	})
 
